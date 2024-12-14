96f5ea052bb0a0157289d906492a03008fcdda03
aboodAldameri <aboodsameer77@gmail.com>
2024-12-15 01:44:58 +0300

    editing

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,9 @@
-
+function massageSubmited(){
+    const show = document.getElementById('massage');
+    const toast = new bootstrap.Toast(show); 
+    toast.show();
+}
+function addNew(photoId) {
+    const mealPhoto = document.getElementById(photoId);
+    mealPhoto.src = "./Photo/photo19.png";
+  }
